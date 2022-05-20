1dc8bac83192c250662b2aa5d907a9b2455f25ca
zuhurialtun <zuhuri312@gmail.com>
2022-05-20 09:49:02 +0300

    file list trying

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="tr">
+<html>
+<head>
+<style>
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+</style>
+</head>
+<body>
+
+<h2>Dosyalar</h2>
+
 <?php
-    ##<h1>Hello World</h1>
+    $array = glob('./*');
+
+    echo '<pre>';
+
+    foreach($array as $path){
+        echo '<a href="'.$path.'">'.substr($path,2).'</a> ';
+    }
 ?>
+
+</body>
+</html>
